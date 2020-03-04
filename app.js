5ab7b5037b64b5a8936aee637700f4a7471b714f
Juan Laporte <juan.laporte@cygrp.com>
2020-03-04 13:32:43 -0700

    revision for episode

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const http = require('http');
+const http = require("http");
 
 const port = process.env.PORT || 3000;
+const testVar = process.env.MY_VAR || "No var present!";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end(testVar);
 });
 
 server.listen(port, () => {
