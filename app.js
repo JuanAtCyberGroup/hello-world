40bce5446b04eb73c12058e43711490edac2de6f
Juan Laporte <juan.laporte@cygrp.com>
2020-03-04 20:34:37 -0700

    added color

--- app.js
+++ app.js
@@ -5,8 +5,26 @@ const testVar = process.env.MY_VAR || "No var present!";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end(testVar);
+  res.setHeader("Content-Type", "text/html; charset=UTF-8");
+  res.end(`<!DOCTYPE html>
+  <html>
+    <head>
+        <style>
+          .header {
+            font-size: 100px;
+            color: #79589F;
+            border-style: solid;
+            border-radius: 2px;
+            margin: 10%;
+          }
+        </style>
+    </head>
+    <body>
+      <div>
+        <H1 class="header"><center>${testVar}</center></H1>
+      </div>
+    </body>
+  </html>`);
 });
 
 server.listen(port, () => {
